de87be24f5d45ae141f97894f6ff969f7a17bdbc
Joe Karaki <j.karaki98@gmail.com>
2019-06-14 17:21:38 -0400

    forgot to save r file

--- nba_prospect_viz.R
+++ nba_prospect_viz.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 
-player_data_2008$X <- NULL
-player_data_2008[28:35] <- list(NULL)
-colnames(player_data_2008) <- c('Yr','Cls','Ht','Player','Team','Conf',
+player_data_2009$X <- NULL
+player_data_2009[28:35] <- list(NULL)
+colnames(player_data_2009) <- c('Yr','Cls','Ht','Player','Team','Conf',
                                 'G','Min%', 'PRPG!','GBPM', 'ORtg', 'Usg', 'eFG', 'TS', 'OR',
                                 'DR','Ast', 'TO','Blk','Stl', 'FTR','FT_raw','FT_pct',
                                 '2P_raw', '2P_pct','3P_raw', '3P_pct' ) 
-final_player<-subset(player_data_2008, !Conf=="")
+final_player<-subset(player_data_2009, !Conf=="")
 i = 1
-while  (i < 5779)
+
+while  (i < nrow(final_player))
 {
   if (!is.na(final_player$Yr[i]))
   {
@@ -33,6 +34,7 @@ draft_data$X <- NULL
 merged <-final_player %>%
   inner_join(draft_data, by=c("Player"))
 
+write.csv(merged,'2009_merged.csv', row.names = FALSE)
 
 
 
