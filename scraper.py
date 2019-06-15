9bc5d10121cc9115f80932788ef9b618b245f9ea
Joe Karaki <j.karaki98@gmail.com>
2019-06-15 00:47:49 -0400

    added some data

--- scraper.py
+++ scraper.py
@@ -39,7 +39,7 @@ def scrape_first(player_links):
         print(count, "...", test_row[3], "...", test_row[4], "...")
 
 
-        if test_row[0] == '2009':
+        if test_row[0] == '2017':
             ############
             # create an empty list to hold all the data
             #player_data_02.append(col)
@@ -79,13 +79,13 @@ def scrape_first(player_links):
 #########################################################
 ####Fetching all URLS for players from a certain year####
 #########################################################
-
+ 
 my_string = "Texas A&M Corpus Chris,Abilene Christian,Air Force,Akron,Alabama,Alabama A&M,Alabama St.,Albany,Alcorn St.,American,Appalachian St.,Arizona,Arizona St.,Arkansas Pine Bluff,Arkansas,Arkansas St.,Army,Auburn,Austin Peay,Ball St.,Baylor,Belmont,Bethune Cookman,Binghamton,Boise St.,Boston College,Boston University,Bowling Green,Bradley,Brown,Bryant,Bucknell,Buffalo,Butler,BYU,Cal Poly,Cal St. Fullerton,California,Cal Baptist,Campbell,Canisius,Central Arkansas,Central Connecticut,Central Michigan,Charleston Southern,Charlotte,Chattanooga,Chicago St.,Cincinnati,Clemson,Cleveland St.,Coastal Carolina,College of Charleston,Colgate,Colorado,Colorado St.,Columbia,Coppin St.,Cornell,Creighton,Cal St. Bakersfield,Cal St. Northridge,Dartmouth,Davidson,Dayton,Delaware,Delaware St.,Denver,DePaul,Detroit,Drake,Drexel,Duke,Duquesne,East Carolina,Eastern Illinois,Eastern Kentucky,Eastern Michigan,Eastern Washington,Elon,East Tennessee St.,Evansville,Fairfield,Fairleigh Dickinson,Florida Gulf Coast,FIU,Florida Atlantic,Florida,Florida A&M,Florida St.,Fordham,Fort Wayne,Fresno St.,Furman,Georgia Southern,Gardner Webb,George Mason,George Washington,Georgetown,Georgia,Georgia St.,Georgia Tech,Gonzaga,Grambling St.,Grand Canyon,Green Bay,Hampton,Hartford,Harvard,Hawaii,High Point,Hofstra,Holy Cross,Houston,Houston Baptist,Howard,Idaho,Idaho St.,Illinois Chicago,Illinois,Illinois St.,Incarnate Word,Indiana,Indiana St.,Iona,Iowa,Iowa St.,IUPUI,Jackson St.,Jacksonville,Jacksonville St.,James Madison,Kansas,Kansas St.,Kennesaw St.,Kent St.,Kentucky,La Salle,Louisiana Monroe,Lafayette,Lamar,Lehigh,Liberty,Lipscomb,Little Rock,LIU Brooklyn,Long Beach St.,Longwood,Louisiana Lafayette,Louisiana Tech,Louisville,Loyola Chicago,Loyola MD,Loyola Marymount,LSU,Maine,Manhattan,Marist,Marquette,Marshall,Maryland,Massachusetts,McNeese St.,Memphis,Mercer,Miami FL,Miami OH,Michigan,Michigan St.,Middle Tennessee,Milwaukee,Minnesota,Mississippi St.,Mississippi Valley St.,Missouri,Missouri St.,Monmouth,Montana,Montana St.,Morehead St.,Morgan St.,Mount St. Mary's,Murray St.,North Carolina A&T,North Carolina Central,Navy,Nebraska,Nevada,New Hampshire,New Mexico,New Mexico St.,New Orleans,Niagara,Nicholls St.,NJIT,Norfolk St.,North Alabama,North Carolina,North Carolina St.,North Dakota,North Dakota St.,North Florida,North Texas,Northeastern,Northern Arizona,Northern Colorado,Northern Illinois,Northern Kentucky,Northwestern,Northwestern St.,Notre Dame,Oakland,Ohio,Ohio St.,Oklahoma,Oklahoma St.,Old Dominion,Mississippi,Nebraska Omaha,Oral Roberts,Oregon,Oregon St.,Pacific,Penn,Penn St.,Pepperdine,Pittsburgh,Portland,Portland St.,Prairie View A&M,Presbyterian,Princeton,Providence,Purdue,Quinnipiac,Radford,Rhode Island,Rice,Richmond,Rider,Robert Morris,Rutgers,Sacramento St.,Sacred Heart,St. Francis PA,Saint Joseph's,Saint Louis,Saint Mary's,Saint Peter's,Sam Houston St.,Samford,San Diego,San Diego St.,San Francisco,San Jose St.,Santa Clara,Savannah St.,Seattle,Seton Hall,Stephen F. Austin,Siena,SIU Edwardsville,SMU,South Alabama,South Carolina,South Carolina St.,South Dakota,South Dakota St.,South Florida,Southeast Missouri St.,Southeastern Louisiana,USC,Southern Illinois,Southern Miss,Southern,Southern Utah,St. Bonaventure,St. Francis NY,St. John's,Stanford,Stetson,Stony Brook,Syracuse,TCU,Temple,Tennessee,Tennessee St.,Tennessee Tech,Texas,Texas A&M,Texas Southern,Texas St.,Texas Tech,The Citadel,Toledo,Towson,Troy,Tulane,Tulsa,UAB,UC Davis,UC Irvine,UC Riverside,UC Santa Barbara,UCF,UCLA,Connecticut,UMass Lowell,UMBC,Maryland Eastern Shore,UMKC,UNC Asheville,UNC Greensboro,UNC Wilmington,Northern Iowa,UNLV,USC Upstate,UT Arlington,Tennessee Martin,Utah,Utah St.,Utah Valley,UTEP,UT Rio Grande Valley,UTSA,Valparaiso,Vanderbilt,VCU,Vermont,Villanova,Virginia,Virginia Tech,VMI,Wagner,Wake Forest,Washington,Washington St.,Weber St.,West Virginia,Western Carolina,Western Illinois,Western Kentucky,Western Michigan,Wichita St.,William & Mary,Winthrop,Wisconsin,Wofford,Wright St.,Wyoming,Xavier,Yale,Youngstown St."
 my_list = my_string.split(",")
 player_links=[]
-url_template_team = "http://www.barttorvik.com/team.php?team={parsed}&year=2009"
+url_template_team = "http://www.barttorvik.com/team.php?team={parsed}&year=2017"
 count = 0
-print(2009)
+print(2017)
 for team in my_list:
     parsed = urllib.parse.quote_plus(team)
     print(count, "...", team)
@@ -98,12 +98,12 @@ for team in my_list:
             links_with_text.append(a['href'])
         
     for b in links_with_text:
-        if 'playerstat.php?year=2009&p' == b[:26]:
+        if 'playerstat.php?year=2017&p' == b[:26]:
             player_links.append(b)
 
 
 import csv
-with open('player_urls_2009', 'w') as myfile:
+with open('player_urls_2017', 'w') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(player_links)
 
@@ -126,7 +126,7 @@ col= ['Yr','Cls','Ht','Player','Team','Conf.',
         '2P_raw', '2P_pct','3P_raw', '3P_pct' ]
 df = pd.DataFrame(merged_data)
 
-df.to_csv('player_data_2009.csv')
+df.to_csv('player_data_2017.csv')
 
 
 
